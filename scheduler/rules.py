de2085058aa1b55ac427bca4f43afd8791dc79ae
aaron carlisle <aaroncarlisle1400@gmail.com>
2025-08-16 19:08:16 -0600

    Base ruleset stubs and test

--- scheduler/rules.py
+++ scheduler/rules.py
@@ -3,8 +3,26 @@ class DefaultRules:
         self.work_start = work_start
         self.work_end = work_end
 
-    def sort(self, events, feedback=None):
+
+    # Highest prio to front
+    def sort_prio(self, events, feedback=None, descending=True):
+        # descending priority sort
+        return sorted(events, key=lambda event: event["priority"], reverse=descending)
+        # raise NotImplementedError("Sort logic not implemented yet.")
+
+    # Highest importance to front
+    def sort_importance(self, events, feedback=None, descending=True):
+        # descending priority sort
+        # return sorted(events, key=lambda event: event["importance"], reverse=descending)
         raise NotImplementedError("Sort logic not implemented yet.")
 
-    def filter(self, events):
+    def sort(self, events, feedback=None, descending=True):
+        # sort based on importance
+        sorted_importance = self.sort_importance(events, descending)
+        # descending priority sort
+        return self.sort_prio(events, descending)
+        # raise NotImplementedError("Sort logic not implemented yet.")
+
+    def filter(self, events, filter):
         raise NotImplementedError("Filter logic not implemented yet.")
+    
--- tests/test_rules.py
+++ tests/test_rules.py
@@ -1,4 +1,4 @@
-from tests.factories import make_event
+from tests.factories import make_event, make_similar_events
 from scheduler.rules import DefaultRules
 from datetime import datetime
 
@@ -6,10 +6,29 @@ def test_sorts_events_by_priority():
     rules = DefaultRules()
     events = [
         make_event("Low", priority=1),
-        make_event("High", priority=5),
+        make_event("Medium", priority=2),
+        make_event("Medium", priority=2),
+        make_event("High", priority=3),
     ]
     sorted_events = rules.sort(events)
     assert sorted_events[0]["name"] == "High"
+    assert sorted_events[1]["name"] == "Medium"
+    assert sorted_events[2]["name"] == "Medium"
+    assert sorted_events[3]["name"] == "Low"
+
+def test_sorts_events_by_importance():
+    rules = DefaultRules()
+    events = [
+        make_event("Low", importance=1),
+        make_event("Medium", importance=2),
+        make_event("High", importance=3),
+        make_event("Medium", importance=2),
+    ]
+    sorted_events = rules.sort(events)
+    assert sorted_events[0]["name"] == "High"
+    assert sorted_events[1]["name"] == "Medium"
+    assert sorted_events[2]["name"] == "Medium"
+    assert sorted_events[3]["name"] == "Low"
 
 def test_breaks_priority_ties_by_earliest_start_time():
     rules = DefaultRules()
@@ -25,3 +44,13 @@ def test_rejects_events_outside_work_hours():
     events = [make_event("Midnight Call", start=datetime(2025,1,1,0,0))]
     filtered_events = rules.filter(events)
     assert len(filtered_events) == 0
+
+# def test_prio_similar_events():
+#     rules = DefaultRules()
+#     similar = make_similar_events()
+#     events = [
+#         similar.get(0),
+#         similar.get(1),
+#     ]
+#     sorted_events = rules.priofilter(events)
+#     assert sorted_events[0]["name"] == "1Prio"
