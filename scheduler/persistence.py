da816a4817a0a3cabc41e67ce75214bf49f9a1e3
aaron carlisle <aaroncarlisle1400@gmail.com>
2025-08-19 16:42:53 -0600

    Db

--- scheduler/persistence.py
+++ scheduler/persistence.py
@@ -4,13 +4,24 @@ from pathlib import Path
 class PreferenceStore:
     def __init__(self, db_path="preferences.db"):
         self.db_path = Path(db_path)
+        self.conn = None
+        self.cursor = None
         self._init_db()
+
 
     def _init_db(self):
         """
-        Create the preferences table if it doesn't exist.
+        Create the users and events table if it doesn't exist.
         """
         raise NotImplementedError("DB init not implemented yet.")
+    
+    def connect_db(self):
+        """
+        Attempt a connection to the database
+        """
+        #Connect to a database file named 'scheduler.db'
+        # conn = sqlite3.connect('scheduler.db')
+        raise NotImplementedError("connect_db not implemented yet.")
 
     def save(self, preferences: dict):
         """
