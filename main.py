7d7cb41eed02da224bf6495df263a5640cda138f
aaron carlisle <aaroncarlisle1400@gmail.com>
2025-09-13 17:22:08 -0600

    Additions to Main

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from ui.view_events import ViewEventsFrame
 # from ui.run_prediction import RunPredictionFrame
 # from ui.reports import ReportsFrame
 
+from db.database_manager import DatabaseManager
+
 # Navigate between different screens for the user
 class SchedulerApp(tk.Tk):
     def __init__(self, username, role):
@@ -16,6 +18,15 @@ class SchedulerApp(tk.Tk):
 
         self.username = username
         self.role = role
+        # Create the shared DatabaseManager instance here
+        self.db_manager = DatabaseManager(
+            host="localhost",
+            user="app_user",
+            password="your_strong_password",
+            database="event_storage_db"
+        )
+        # set up connection to DB at begin of program run
+        self.db_manager.connect_db()
 
         # Container for all frames
         container = tk.Frame(self)
@@ -39,6 +50,15 @@ class SchedulerApp(tk.Tk):
         frame = self.frames[frame_class]
         frame.tkraise()
 
+    def on_close(self):
+        """Handles closing the application and the database connection."""
+        print("Closing the program")
+        if self.db_manager:
+            self.db_manager.close_db()
+        self.destroy()
+
 if __name__ == "__main__":
     app = SchedulerApp(username="demo_user", role="Normal User")
+    # Ensure the connection is closed when the window is closed by force or on x out
+    app.protocol("WM_DELETE_WINDOW", app.on_close)
     app.mainloop()
