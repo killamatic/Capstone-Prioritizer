dd50dfa4664168668604f52821cb4a9e7ed42dcf
aaron carlisle <aaroncarlisle1400@gmail.com>
2025-10-08 14:22:05 -0600

    default ruleset updated and cleaned

--- scheduler/rules.py
+++ scheduler/rules.py
@@ -8,16 +8,21 @@ class DefaultRules:
 
     """
     Sorting by compound sort.
+    events: ['event_id', 'event_name', 'event_date', 'event_duration', 'predicted_priority', 
+        'actual_priority', 'participant_count', 'created_at', 'created_by']
+    
     """
-    def sort(self, events, feedback=None, descending=True):
+    def sort(self, events, descending=True):
         # sort based on importance
         # sorted_importance = self.sort_importance(events, descending)
         # # descending priority sort
         # sorted_imp_prio = self.sort_prio(sorted_importance, descending)
-
+        actual_priority_index = 5
+        participant_count_index = 6
         sorted_events = sorted(
             events, 
-            key=lambda x: (-x['priority'], -x['importance'], x['start'])
+            # key=lambda x: (-x['actual_priority'], -x['participant_count'], x['start'])
+            key=lambda x: (-x[actual_priority_index], -x[participant_count_index])
         )
 
         # return sorted_imp_prio
